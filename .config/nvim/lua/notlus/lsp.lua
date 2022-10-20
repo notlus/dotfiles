f114980811d54cb9a623d043c6fd82525389bf6d
Jeffrey Sulton <notlus68@me.com>
2022-10-20 16:40:18 -0700

    Fix breaking change
    Change `update_capabilities` to `default_capabilities`.

--- .config/nvim/lua/notlus/lsp.lua
+++ .config/nvim/lua/notlus/lsp.lua
@@ -36,7 +36,7 @@ end
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Setup lspconfig
 
