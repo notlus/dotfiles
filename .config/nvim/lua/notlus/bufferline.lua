9d8537b91b3e2bb2a1c4df9948ee91db9c96c3c9
Jeffrey Sulton <jeffrey.sulton@dtexsystems.com>
2023-06-02 16:20:30 -0700

    Update bufferline setup function

--- .config/nvim/lua/notlus/bufferline.lua
+++ .config/nvim/lua/notlus/bufferline.lua
@@ -1,8 +1,9 @@
 require("bufferline").setup {
-  diagnostics = "nvim_lsp",
-
-  numbers = function(opts)
-    return string.format('%s·%s', opts.raise(opts.id), opts.lower(opts.ordinal))
-  end,
+    options = {
+        diagnostics = "nvim_lsp",
+        numbers = function(opts)
+            return string.format('%s·%s', opts.raise(opts.id), opts.lower(opts.ordinal))
+        end,
+    }
 }
  
