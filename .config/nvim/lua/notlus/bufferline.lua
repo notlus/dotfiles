9171ac19bba2ff7507cee1695a2973ef6d1ae199
Jeffrey Sulton <jeffrey.sulton@dtexsystems.com>
2023-12-08 20:50:29 -0800

    Update bufferline settings

--- .config/nvim/lua/notlus/bufferline.lua
+++ .config/nvim/lua/notlus/bufferline.lua
@@ -1,9 +1,14 @@
 require("bufferline").setup {
     options = {
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        max_name_length = 25,
         diagnostics = "nvim_lsp",
-        numbers = function(opts)
-            return string.format('%s·%s', opts.raise(opts.id), opts.lower(opts.ordinal))
+        diagnostics_indicator = function(count, level)
+            local icon = level:match("error") and " " or ""
+            return " " .. icon .. count
         end,
+        numbers = "none"
     }
 }
  
