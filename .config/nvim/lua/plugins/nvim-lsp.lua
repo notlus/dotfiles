d050eba79ba9c21befbb5f06ce6897ac01f31a93
Jeffrey Sulton <jeffrey.sulton@dtexsystems.com>
2023-12-11 21:08:30 -0800

    Fix LSP config

--- .config/nvim/lua/plugins/nvim-lsp.lua
+++ .config/nvim/lua/plugins/nvim-lsp.lua
@@ -1,40 +1,82 @@
 -- nvim-lsp.lua
 return {
-  "neovim/nvim-lspconfig",
-  requires = {
-    "ray-x/lspsaga.nvim",
-  },
-  after = "cmp-nvim-lsp",
-  event = "BufReadPre",
-  ft = { 'c', 'cpp', 'objc', 'objcpp', 'dart', 'kotlin', 'lua', 'swift' },
+    "neovim/nvim-lspconfig",
+    requires = {
+        "ray-x/lspsaga.nvim",
+    },
+    after = "cmp-nvim-lsp",
+    event = "BufReadPre",
+    ft = { "c", "cpp", "objc", "objcpp", "dart", "kotlin", "lua", "swift" },
 
-  config = function()
-    local lspconfig = require("lspconfig")
+    config = function() -- Global mappings.
+        -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+        vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+        vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+        vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+        --
+        -- Use LspAttach autocommand to only map the following keys
+        -- after the language server attaches to the current buffer
+        vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+            callback = function(ev)
+                -- Enable completion triggered by <c-x><c-o>
+                vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-    local servers = { "clangd", "sourcekit", "dartls", "kotlin_language_server", "lua_ls" }
-    local function setup_servers()
-      for _, server in ipairs(servers) do
-        local opts = {} -- options table, can be customized per server
-        if server == "lua-ls" then
-          opts = {
-            settings = {
-              Lua = {
-                diagnostics = {
-                  -- Get the language server to recognize the `vim` global
-                  globals = {"vim"},
-                },
-                workspace = {
-                  -- Make the server aware of Neovim runtime files
-                  library = vim.api.nvim_get_runtime_file("", true),
-                },
-              },
-            },
-          }
+                -- Buffer local mappings.
+                -- See `:help vim.lsp.*` for documentation on any of the below functions
+                local opts = { buffer = ev.buf }
+                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+                vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+                vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+                vim.keymap.set("n", "<space>wl", function()
+                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                end, opts)
+                vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+                vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+                vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                vim.keymap.set("n", "<space>f", function()
+                    vim.lsp.buf.format({ async = true })
+                end, opts)
+            end,
+        })
+
+        local lspconfig = require("lspconfig")
+
+        -- Add additional capabilities supported by nvim-cmp
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+        local servers = { "clangd", "sourcekit", "dartls", "kotlin_language_server", "lua_ls" }
+        local function setup_servers()
+            for _, server in ipairs(servers) do
+                capabilities = capabilities
+                local opts = {} -- options table, can be customized per server
+                if server == "lua_ls" then
+                    opts = {
+                        settings = {
+                            Lua = {
+                                diagnostics = {
+                                    -- Get the language server to recognize the `vim` global
+                                    globals = { "vim" },
+                                },
+                                workspace = {
+                                    -- Make the server aware of Neovim runtime files
+                                    library = vim.api.nvim_get_runtime_file("", true),
+                                },
+                            },
+                        },
+                    }
+                end
+                lspconfig[server].setup(opts)
+            end
         end
-        lspconfig[server].setup(opts)
-      end
-    end
 
-    setup_servers()
-  end,
+        setup_servers()
+    end,
 }
