32b932ee75a762b90cfd8da5139dba8a15d0ebd0
Jeffrey Sulton <jeffrey.sulton@dtexsystems.com>
2023-12-11 21:09:09 -0800

    Fix xcodebuild initialization

--- .config/nvim/lua/plugins/xcodebuild.lua
+++ .config/nvim/lua/plugins/xcodebuild.lua
@@ -1,3 +1,11 @@
 return {
-  "wojciech-kulik/xcodebuild.nvim"
+	"wojciech-kulik/xcodebuild.nvim",
+	config = function()
+		require("xcodebuild").setup({
+			-- Set to true to enable debug output
+			debug = false,
+		})
+
+		ft = { "objc", "objc++", "swift", "c", "cpp" }
+	end,
 }
