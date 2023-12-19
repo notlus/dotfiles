41163257620dda433c76a7fa84c1bb8e11eb98f9
Jeffrey Sulton <jeffrey.sulton@dtexsystems.com>
2023-12-18 19:48:37 -0800

    Update noice configuration

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,10 +1,24 @@
-  return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-    },
-    dependencies = {
-     "MunifTanjim/nui.nvim",
-     "rcarriga/nvim-notify",
-     }
-  }
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+	config = function()
+		require("noice").setup({
+			notify = {
+				enabled = false,
+				view = "notify",
+			},
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = true, -- add a border to hover docs and signature help
+			},
+		})
+	end,
+}
