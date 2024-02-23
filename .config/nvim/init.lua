1effef12c0d0e2037725d05e89ea907d3fd6ccfc
Jeffrey Sulton <jeffrey.sulton@dtexsystems.com>
2024-02-22 20:37:42 -0800

    Highlight on yank

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -50,3 +50,12 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.commentstring = "// %s"
 	end,
 })
+
+-- Highlight on yank
+-- See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
