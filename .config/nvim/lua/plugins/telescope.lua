e4ba1ae35432b128910b7e40491895a3f5a8647e
Jeffrey Sulton <jeffrey.sulton@dtexsystems.com>
2024-02-22 20:46:50 -0800

    Add ui-select and format

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,23 @@
 return {
 	"nvim-telescope/telescope.nvim",
+	event = "VeryLazy",
 	dependencies = {
+		"nvim-lua/plenary.nvim",
 		"nvim-telescope/telescope-fzf-native.nvim",
+		{ "nvim-telescope/telescope-ui-select.nvim" },
 		build = "make",
-		config = function()
-			require("telescope").load_extension("fzf")
-		end,
 	},
+	config = function()
+		require("telescope").setup({
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown(),
+				},
+			},
+		})
+
+		pcall(require("telescope").load_extension, "fzf")
+		pcall(require("telescope").load_extension, "ui-select")
+	end,
 	cmd = { "Telescope" },
 }
