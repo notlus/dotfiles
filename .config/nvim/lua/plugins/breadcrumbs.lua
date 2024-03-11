87ed75a822f826f6d1d7fb7458d741274c8cf85f
Jeffrey Sulton <notlus68@me.com>
2024-03-11 09:37:01 -0700

    Add ufo and breadcrumbs

---
+++ .config/nvim/lua/plugins/breadcrumbs.lua
@@ -0,0 +1,12 @@
+return {
+	"LunarVim/breadcrumbs.nvim",
+	dependencies = {
+		{ "SmiteshP/nvim-navic" },
+	},
+    opts = {
+        -- event = "BufEnter",
+    },
+	-- config = function()
+	-- 	require("breadcrumbs").setup()
+	-- end,
+}
