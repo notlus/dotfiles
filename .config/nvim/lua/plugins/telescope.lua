05872e325f6dc3c0c99e3c355204342bc7bdbc67
Jeffrey Sulton <notlus68@me.com>
2024-03-11 09:33:28 -0700

    Try the  theme

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -8,10 +8,49 @@ return {
 		build = "make",
 	},
 	config = function()
+		local icons = require("core.icons")
+
 		require("telescope").setup({
+			defaults = {
+				prompt_prefix = icons.ui.Telescope .. " ",
+				selection_caret = icons.ui.Forward .. " ",
+				entry_prefix = "   ",
+				initial_mode = "insert",
+				selection_strategy = "reset",
+				path_display = { "smart" },
+				color_devicons = true,
+			},
 			extensions = {
 				["ui-select"] = {
 					require("telescope.themes").get_dropdown(),
+				},
+				fzf = {
+					fuzzy = true, -- false will only do exact matching
+					override_generic_sorter = true, -- override the generic sorter
+					override_file_sorter = true, -- override the file sorter
+					case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+				},
+			},
+			pickers = {
+				live_grep = {
+					theme = "dropdown",
+				},
+				grep_string = {
+					theme = "dropdown",
+				},
+				find_files = {
+					theme = "dropdown",
+					previewer = false,
+				},
+				buffers = {
+					theme = "dropdown",
+					previewer = false,
+				},
+				lsp_references = {
+					theme = "dropdown",
+				},
+				lsp_document_symbols = {
+					theme = "dropdown",
 				},
 			},
 		})
