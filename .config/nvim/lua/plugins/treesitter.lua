a01ce2cc0378b26f82c8f12d47af01631216911a
Jeffrey Sulton <jeffrey.sulton@dtexsystems.com>
2024-04-19 08:53:35 -0700

    Formatting

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,24 @@
 return {
- 'nvim-treesitter/nvim-treesitter',
-  event = 'VeryLazy',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-  },
-build = ':TSUpdate',
-  opts = {
-    highlight = {
-      enable = true,
-    },
-    indent = { enable = true },
-    auto_install = true, -- automatically install syntax support when entering new file type buffer
-    ensure_installed = {
-      'lua',
-    },
-  },
-  config = function (_, opts)
-    local configs = require("nvim-treesitter.configs")
-    configs.setup(opts)
-  end
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	build = ":TSUpdate",
+	opts = {
+		highlight = {
+			enable = true,
+		},
+		indent = { enable = true },
+		auto_install = true, -- automatically install syntax support when entering new file type buffer
+		ensure_installed = {
+			"lua",
+		},
+	},
+	config = function(_, opts)
+		local configs = require("nvim-treesitter.configs")
+		configs.setup(opts)
+	end,
 }
 --[[
     require'nvim-treesitter.configs'.setup {
