a3817f2afdf4f153d3e63abe849e0d18855201f3
Jeffrey Sulton <jeffrey.sulton@dtexsystems.com>
2024-04-19 08:52:35 -0700

    Use opts table

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,19 +1,17 @@
 return {
 	"akinsho/bufferline.nvim",
 	event = "BufWinEnter",
-	config = function()
-		require("bufferline").setup({
-			options = {
-				show_buffer_close_icons = false,
-				show_close_icon = false,
-				max_name_length = 25,
-				diagnostics = "nvim_lsp",
-				diagnostics_indicator = function(count, level)
-					local icon = level:match("error") and " " or ""
-					return " " .. icon .. count
-				end,
-				numbers = "none",
-			},
-		})
-	end,
+	opts = {
+		options = {
+			show_buffer_close_icons = false,
+			show_close_icon = false,
+			max_name_length = 25,
+			diagnostics = "nvim_lsp",
+			diagnostics_indicator = function(count, level)
+				local icon = level:match("error") and " " or ""
+				return " " .. icon .. count
+			end,
+			numbers = "none",
+		},
+	},
 }
