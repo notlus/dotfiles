0e90db9d8874d56c6831e081f3f6e297bacab454
Jeffrey Sulton <jeffrey.sulton@dtexsystems.com>
2024-09-19 10:30:06 -0700

    Add support for using Option-Left and Option-Right to skip words

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -12,4 +12,10 @@ config.color_scheme = "Tokyo Night"
 config.font_size = 11.0
 config.enable_tab_bar = false
 -- config.macos_window_background_blur = 10
+config.keys = {
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+  }
 return config
