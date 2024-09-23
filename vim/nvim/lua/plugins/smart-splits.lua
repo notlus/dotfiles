ec41b5d2a07e82fad863e8ac15d16385c7299377
Jeffrey Sulton <notlus68@me.com>
2024-09-22 21:10:44 -0700

    Add key maps for moving between splits.

--- vim/nvim/lua/plugins/smart-splits.lua
+++ vim/nvim/lua/plugins/smart-splits.lua
@@ -2,6 +2,7 @@ return {
 	"mrjones2014/smart-splits.nvim",
 	lazy = false,
 	keys = {
+		-- Moving between splits
 		{
 			"<C-h>",
 			function()
@@ -33,6 +34,40 @@ return {
 			end,
 			mode = { "i", "n", "v" },
 			desc = "Move to right window",
+		},
+
+		-- Resizing splits
+		{
+			"<C-Up>",
+			function()
+				require("smart-splits").resize_up()
+			end,
+			mode = { "i", "n", "v" },
+			desc = "Resize up",
+		},
+		{
+			"<C-Down>",
+			function()
+				require("smart-splits").resize_down()
+			end,
+			mode = { "i", "n", "v" },
+			desc = "Resize down",
+		},
+		{
+			"<C-Left>",
+			function()
+				require("smart-splits").resize_left()
+			end,
+			mode = { "i", "n", "v" },
+			desc = "Resize left",
+		},
+		{
+			"<C-Right>",
+			function()
+				require("smart-splits").resize_right()
+			end,
+			mode = { "i", "n", "v" },
+			desc = "Resize right",
 		},
 	},
 }
