7a02afed8b48a7be9b568b3a61f852992b1ab7b0
Jeffrey Sulton <jeffrey.sulton@dtexsystems.com>
2024-09-23 17:54:13 -0700

    Add tab bar

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -46,8 +46,8 @@ config.default_cursor_style = "BlinkingBar"
 config.font = wezterm.font("FiraCode Nerd Font Mono")
 config.color_scheme = "Tokyo Night"
 config.font_size = 11.0
-config.enable_tab_bar = false
--- config.macos_window_background_blur = 10
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
 config.keys = {
 	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
 	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
