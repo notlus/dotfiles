20c5b4a8a94cf88779474c8216dea5ed6a54f572
Jeffrey Sulton <notlus68@me.com>
2024-11-02 09:55:45 -0700

    Add keymap and disable logging

--- vim/nvim/lua/plugins/supermaven.lua
+++ vim/nvim/lua/plugins/supermaven.lua
@@ -1,6 +1,11 @@
 return {
 	"supermaven-inc/supermaven-nvim",
 	config = function()
-		require("supermaven-nvim").setup({})
+		require("supermaven-nvim").setup({
+			keymaps = {
+				accept_suggestion = "<Tab>",
+			},
+            log_level = "off",
+		})
 	end,
 }
