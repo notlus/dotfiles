4ffdcb52e3e8dde4ec3e3485630db0d40e9b5b0f
Jeffrey Sulton <notlus68@me.com>
2024-12-28 10:34:42 -0800

    Add initial blink.cmp config

---
+++ vim/nvim/lua/plugins/blink.lua
@@ -0,0 +1,128 @@
+return {
+	"saghen/blink.cmp",
+	-- "onsails/lspkind.nvim",
+	dependencies = {
+		"saghen/blink.compat",
+		{
+			"saghen/blink.compat",
+			"giuxtaposition/blink-cmp-copilot",
+			-- use the latest release, via version = '*', if you also use the latest release for blink.cmp
+			version = "*",
+			-- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+			lazy = true,
+			-- make sure to set opts so that lazy.nvim calls blink.compat's setup
+			opts = {},
+		},
+	},
+
+	version = "v0.*",
+	event = "InsertEnter",
+	opts = {
+		-- 'default' for mappings similar to built-in completion
+		-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+		-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+		-- see the "default configuration" section below for full documentation on how to define
+		-- your own keymap.
+		keymap = { preset = "super-tab" },
+
+		appearance = {
+			use_nvim_cmp_as_default = false,
+			nerd_font_variant = "mono",
+			kind_icons = {
+				Copilot = "",
+				Supermaven = "",
+				Text = "󰉿",
+				Method = "󰊕",
+				Function = "󰊕",
+				Constructor = "󰒓",
+
+				Field = "󰜢",
+				Variable = "󰆦",
+				Property = "󰖷",
+
+				Class = "󱡠",
+				Interface = "󱡠",
+				Struct = "󱡠",
+				Module = "󰅩",
+
+				Unit = "󰪚",
+				Value = "󰦨",
+				Enum = "󰦨",
+				EnumMember = "󰦨",
+
+				Keyword = "󰻾",
+				Constant = "󰏿",
+
+				Snippet = "󱄽",
+				Color = "󰏘",
+				File = "󰈔",
+				Reference = "󰬲",
+				Folder = "󰉋",
+				Event = "󱐋",
+				Operator = "󰪚",
+				TypeParameter = "󰬛",
+			},
+		},
+
+		sources = {
+			default = { "lsp", "path", "buffer", "snippets", "supermaven" },
+			providers = {
+				supermaven = {
+					name = "supermaven",
+					module = "blink.compat.source",
+					score_offset = 100,
+					async = true,
+					transform_items = function(_, items)
+						local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+						local kind_idx = #CompletionItemKind + 1
+						CompletionItemKind[kind_idx] = "Supermaven"
+						for _, item in ipairs(items) do
+							item.kind = kind_idx
+						end
+						return items
+					end,
+				},
+				copilot = {
+					name = "copilot",
+					module = "blink-cmp-copilot",
+					score_offset = 100,
+					async = true,
+					transform_items = function(_, items)
+						local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+						local kind_idx = #CompletionItemKind + 1
+						CompletionItemKind[kind_idx] = "Copilot"
+						for _, item in ipairs(items) do
+							item.kind = kind_idx
+						end
+						return items
+					end,
+				},
+			},
+		},
+
+		signature = {
+			enabled = true,
+			window = {
+				border = "single",
+			},
+		},
+		completion = {
+			ghost_text = { enabled = true },
+			menu = {
+				border = "single",
+			},
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 200,
+				window = {
+					border = "single",
+				},
+			},
+		},
+	},
+	opts_extend = {
+		"sources.completion.enabled_providers",
+		"sources.compat",
+		"sources.default",
+	},
+}
