d7d49f63ba6e4add277b2015f0dbd212e90ea1f3
Jeffrey Sulton <notlus68@me.com>
2024-12-28 12:23:53 -0800

    Re-add copilot.lua

---
+++ vim/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,16 @@
+return {
+	"zbirenbaum/copilot.lua",
+	event = "InsertEnter",
+	config = function()
+		-- Setting up copilot
+		vim.defer_fn(function()
+			require("copilot").setup({
+				suggestion = { enabled = false },
+				panel = { enabled = false },
+			})
+		end, 100) -- Defer the setup to ensure the plugin is fully loaded; adjust the delay as needed
+
+		-- Setting up copilot-cmp integration
+		-- require("copilot_cmp").setup()
+	end,
+}
