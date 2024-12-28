8367798aaf9eee1e9e07a9856fa882fb43e31614
Jeffrey Sulton <notlus68@me.com>
2024-12-28 10:26:44 -0800

    Remove the accept_suggestion keymap

--- vim/nvim/lua/plugins/supermaven.lua
+++ vim/nvim/lua/plugins/supermaven.lua
@@ -1,11 +1,11 @@
 return {
 	"supermaven-inc/supermaven-nvim",
+	opts = {
+		disable_inline_completions = true,
+	},
 	config = function()
 		require("supermaven-nvim").setup({
-			keymaps = {
-				accept_suggestion = "<Tab>",
-			},
-            log_level = "off",
+			log_level = "off",
 		})
 	end,
 }
