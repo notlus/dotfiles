9229f21e39655c61c9554c9bc35a42126152f7fc
Jeffrey Sulton <notlus68@me.com>
2024-12-28 12:33:25 -0800

    Show hidden files by default

--- vim/nvim/lua/plugins/oil.lua
+++ vim/nvim/lua/plugins/oil.lua
@@ -1,6 +1,9 @@
 return {
 	"stevearc/oil.nvim",
-	opts = {},
-	-- Optional dependencies
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		view_options = {
+			show_hidden = true,
+		},
+	},
 }
