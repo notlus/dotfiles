2ba259c5155ab8932dd3bdda543758788725d374
Jeffrey Sulton <notlus68@me.com>
2025-01-01 21:31:08 -0800

    Fix auto-session warnings

--- vim/nvim/lua/plugins/auto-session.lua
+++ vim/nvim/lua/plugins/auto-session.lua
@@ -4,9 +4,9 @@ return {
 		local auto_session = require("auto-session")
 
 		auto_session.setup({
-			auto_restore_enabled = true,
-			auto_session_suppress_dirs = { "~/Downloads", "~/Documents", "~/Desktop" },
-			auto_save_enabled = true,
+			auto_restore = true,
+			suppress_dirs = { "~/Downloads", "~/Documents", "~/Desktop" },
+			auto_save = true,
 		})
 	end,
 }
