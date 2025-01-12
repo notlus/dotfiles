2df812c6b8fb85206792419c82ba770807c3ebda
Jeffrey Sulton <notlus68@me.com>
2025-01-11 18:59:52 -0800

    Add left padding to label

--- sketchybar/.config/sketchybar/items/front_app.sh
+++ sketchybar/.config/sketchybar/items/front_app.sh
@@ -16,18 +16,8 @@ sketchybar --add item  front_app left \
                        background.drawing=off \
                        background.color="$BAR_COLOR" \
                        label.color="$FRONT_APP_LABEL_COLOR" \
-                       label.padding_left=0 \
+                       label.padding_left=5 \
                        label.padding_right=0 \
                        icon.color=$FRONT_APP_LABEL_COLOR \
                        icon.font="sketchybar-app-font:Regular:16.0" \
                        script="$PLUGIN_DIR/front_app.sh"
-
-# sketchybar --add item front_app_spacer_right left \
-#            --set      front_app_spacer_right \
-#                       background.drawing=off
-#
-# sketchybar --add bracket front_app_bracket front_app_spacer_left front_app front_app_spacer_right \
-#            --set         front_app_bracket \
-#                          background.border_color="$FRONT_APP_BACKGROUND_BORDER_COLOR" \
-#                          background.corner_radius="$BRACKET_BACKGROUND_CORNER_RADIUS" \
-#                          background.border_width="$BRACKET_BACKGROUND_BORDER_WIDTH"
