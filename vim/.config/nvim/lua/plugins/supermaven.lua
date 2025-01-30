363c70c9c14da244d07b099fb3bea4070a184c5b
Jeffrey Sulton <notlus68@me.com>
2025-01-30 08:10:22 -0800

    Add `disable_keymaps = true`

--- vim/.config/nvim/lua/plugins/supermaven.lua
+++ vim/.config/nvim/lua/plugins/supermaven.lua
@@ -2,6 +2,7 @@ return {
 	"supermaven-inc/supermaven-nvim",
 	opts = {
 		disable_inline_completions = true,
+        disable_keymaps = true,
 	},
 	config = function()
 		require("supermaven-nvim").setup({
