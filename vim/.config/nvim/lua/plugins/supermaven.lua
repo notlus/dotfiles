1ccf907e80efb1ebe7639af7fae60c265f9f9359
Jeffrey Sulton <notlus68@me.com>
2025-02-15 20:11:11 -0800

    Fix launching Things

--- vim/.config/nvim/lua/plugins/supermaven.lua
+++ vim/.config/nvim/lua/plugins/supermaven.lua
@@ -1,8 +1,8 @@
 return {
 	"supermaven-inc/supermaven-nvim",
 	opts = {
-		disable_inline_completions = true,
-        disable_keymaps = true,
+		-- disable_inline_completions = true,
+		--       disable_keymaps = true,
 	},
 	config = function()
 		require("supermaven-nvim").setup({
