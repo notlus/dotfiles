99a9f71500efc232078d9b44320f4a775cbab263
Jeffrey Sulton <notlus68@me.com>
2025-07-05 10:37:29 -0700

    Consolidate gitsigns keymaps into plugin config
    - Move all gitsigns keymaps from keymaps.lua to gitsigns.lua
    - Use on_attach for buffer-local keymaps with lazy loading
    - Add new keymaps: <leader>hS to stage entire file, <leader>tb to toggle
    blame
    - Add visual mode support for staging/resetting selected lines
    - Improve navigation with proper diff mode handling

--- vim/.config/nvim/lua/plugins/gitsigns.lua
+++ vim/.config/nvim/lua/plugins/gitsigns.lua
@@ -52,6 +52,60 @@ return {
 				row = 0,
 				col = 1,
 			},
+			on_attach = function(bufnr)
+				local gs = package.loaded.gitsigns
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Navigation
+				map("n", "]c", function()
+					if vim.wo.diff then
+						return "]c"
+					end
+					vim.schedule(function()
+						gs.next_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true, silent = true, desc = "Jump to the next Git change" })
+
+				map("n", "[c", function()
+					if vim.wo.diff then
+						return "[c"
+					end
+					vim.schedule(function()
+						gs.prev_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true, silent = true, desc = "Jump to the previous Git change" })
+
+				-- Actions
+				map("n", "hs", gs.stage_hunk, { silent = true, desc = "Stage the current hunk" })
+				map("n", "hr", gs.reset_hunk, { silent = true, desc = "Reset the current hunk" })
+				map("v", "hs", function()
+					gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, { silent = true, desc = "Stage selected lines" })
+				map("v", "hr", function()
+					gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, { silent = true, desc = "Reset selected lines" })
+
+				-- New keymaps you requested
+				map("n", "<leader>hS", gs.stage_buffer, { desc = "Stage entire file" })
+				map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "Toggle blame line" })
+
+				map("n", "hu", gs.undo_stage_hunk, { silent = true, desc = "Undo the last staged hunk" })
+				map("n", "hp", gs.preview_hunk, { silent = true, desc = "Preview the changes in the current hunk" })
+				map("n", "bl", function()
+					gs.blame_line({ full = true })
+				end, { silent = true, desc = "Blame the line under the cursor" })
+				map("n", "bd", gs.diffthis, { silent = true, desc = "Show a diff of the current file" })
+
+				-- Text object
+				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { silent = true, desc = "Select hunk" })
+			end,
 		})
 	end,
 }
