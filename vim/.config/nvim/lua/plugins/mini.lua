8df729c06510e4bbd13b69626bfcf9e82b0d2598
Jeffrey Sulton <jeffrey.sulton@dtexsystems.com>
2025-09-14 22:14:12 -0700

    refactor(mini.nvim): simplify session management to use local hidden files
    Switch from project-based sessions to local .nvim.session files:
      - Use hidden .nvim.session files instead of Session.vim to avoid git
    tracking
      - Remove complex project name handling and auto-load on startup
      - Simplify keymaps to <leader>Sw (save), <leader>Sr (load), <leader>Sd
    (delete)
      - Auto-save only updates existing sessions, won't create new ones
    automatically

--- vim/.config/nvim/lua/plugins/mini.lua
+++ vim/.config/nvim/lua/plugins/mini.lua
@@ -8,57 +8,46 @@ return {
 		require("mini.files").setup()
 		require("mini.icons").setup()
 		require("mini.sessions").setup({
-			autoread = false, -- Whether to read latest session if Neovim started without file arguments
-			autowrite = false, -- Disable autowrite, we'll handle it manually
-			directory = vim.fn.stdpath("data") .. "/sessions/", -- Directory where global sessions are stored
-			file = "", -- Disable local session files
+			autoread = false, -- Don't auto-read sessions on startup
+			autowrite = false, -- Don't auto-write sessions
+			directory = "", -- Disable global sessions directory
+			file = ".nvim.session", -- Use hidden local session files
 		})
 
-		-- Function to get project root (you can adjust the root patterns as needed)
-		local function get_project_name()
-			local project_directory = vim.fn.getcwd()
-			return project_directory:gsub("/", "%%")
-		end
+		local MiniSessions = require("mini.sessions")
+		local session_file = MiniSessions.config.file
 
-		-- Session keymaps
-		-- Quick save/load for current project
-		vim.keymap.set("n", "<leader>sW", function()
-			MiniSessions.write(get_project_name(), { force = true })
-			vim.notify("Session saved for " .. vim.fn.getcwd())
-		end, { desc = "Save project session" })
+		-- Save current session to local Session.vim
+		vim.keymap.set("n", "<leader>Sw", function()
+			MiniSessions.write(session_file, { force = true })
+			vim.notify("Session saved")
+		end, { desc = "Save session" })
 
-		vim.keymap.set("n", "<leader>sR", function()
-			MiniSessions.read(get_project_name(), {})
-		end, { desc = "Load project session" })
+		-- Load session from local Session.vim
+		vim.keymap.set("n", "<leader>Sr", function()
+			if vim.fn.filereadable(session_file) == 1 then
+				MiniSessions.read(session_file, { force = true })
+				vim.notify("Session loaded")
+			else
+				vim.notify("No " .. session_file .. " found")
+			end
+		end, { desc = "Load session" })
 
-		-- Original keymaps for named sessions
-		-- vim.keymap.set('n', '<leader>ss', function() MiniSessions.write(vim.fn.input('Session name: ')) end, { desc = 'Save named session' })
-		-- vim.keymap.set('n', '<leader>sl', function() MiniSessions.select() end, { desc = 'Select session' })
-		-- vim.keymap.set('n', '<leader>sd', function() MiniSessions.delete(vim.fn.input('Delete session: ')) end, { desc = 'Delete session' })
+		-- Delete local session
+		vim.keymap.set("n", "<leader>Sd", function()
+			if vim.fn.filereadable(session_file) == 1 then
+				MiniSessions.delete(session_file, { force = true })
+				vim.notify("Session deleted")
+			else
+				vim.notify("No " .. session_file .. " to delete")
+			end
+		end, { desc = "Delete session" })
 
-		-- Auto-save session on exit for current project
+		-- Auto-save session on exit
 		vim.api.nvim_create_autocmd("VimLeavePre", {
 			callback = function()
-				MiniSessions.write(get_project_name(), { force = true })
-			end,
-		})
-
-		-- Auto-load session when entering Neovim without arguments
-		vim.api.nvim_create_autocmd("VimEnter", {
-			callback = function()
-				if vim.fn.argc() == 0 then
-					vim.schedule(function()
-						local ok = pcall(function()
-							MiniSessions.read(get_project_name(), {})
-						end)
-						if ok then
-							-- Force syntax highlighting and filetype detection for current buffer
-							vim.cmd("filetype detect")
-							vim.cmd("syntax enable")
-							-- Also trigger BufRead autocmds which might set up highlighting
-							vim.cmd("doautocmd BufRead")
-						end
-					end)
+				if vim.fn.filereadable(session_file) == 1 then
+					MiniSessions.write(session_file, { force = true })
 				end
 			end,
 		})
