9e89af378a4440e3cb11caa50cfa9ac77391916b
Jeffrey Sulton <jeffrey.sulton@dtexsystems.com>
2025-10-20 10:06:38 -0700

    Update Xcode version

---
+++ vim/.config/nvim/lua/plugins/amazonq.lua
@@ -0,0 +1,37 @@
+return {
+	{
+		name = "amazonq",
+		url = "https://github.com/awslabs/amazonq.nvim.git",
+		inline_suggest = false,
+		opts = {
+			ssoStartUrl = "https://d-9067cfa456.awsapps.com/start",
+			filetypes = {
+				"amazonq",
+				"bash",
+				"python",
+				"typescript",
+				"javascript",
+				"ruby",
+				"sh",
+				"sql",
+				"c",
+				"cpp",
+				"go",
+				"rust",
+				"lua",
+				"swift",
+			},
+			on_chat_open = function()
+				vim.cmd([[
+                    vertical botright split
+                    vertical resize 80
+                    set wrap
+                    set breakindent
+                    set nonumber
+                    set norelativenumber
+                    set nolist
+                ]])
+			end,
+		},
+	},
+}
