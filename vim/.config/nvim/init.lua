88e6fdec5044557e0a83cf826a8e1d28675f1202
Jeffrey Sulton <jeffrey.sulton@dtexsystems.com>
2025-10-20 10:55:42 -0700

    Use vim.hl.on_yank()

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -49,11 +49,10 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 -- Highlight on yank
--- See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
 	group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
 	callback = function()
-		vim.highlight.on_yank()
+		vim.hl.on_yank()
 	end,
 })
 
