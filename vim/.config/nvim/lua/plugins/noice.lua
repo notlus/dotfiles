5fd0bfd44bac3c458f8b0e192283ff21c83fdfff
Jeffrey Sulton <jeffrey.sulton@dtex.ai>
2026-02-16 10:47:37 -0800

    refactor(nvim): simplify noice.nvim config and use classic cmdline
    - Replace config function with declarative opts table
    - Switch cmdline view from popup to classic "cmdline" style
    - Disable command_palette preset
    - Condense route definitions to single lines

--- vim/.config/nvim/lua/plugins/noice.lua
+++ vim/.config/nvim/lua/plugins/noice.lua
@@ -1,42 +1,29 @@
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	opts = {},
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"rcarriga/nvim-notify",
-	},
-	config = function()
-		require("noice").setup({
-			notify = {
-				enabled = false,
-				view = "notify",
-			},
-			presets = {
-				bottom_search = true, -- use a classic bottom cmdline for search
-				command_palette = true, -- position the cmdline and popupmenu together
-				long_message_to_split = true, -- long messages will be sent to a split
-				inc_rename = false, -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = true, -- add a border to hover docs and signature help
-			},
-			routes = {
-				{
-					filter = {
-						event = "msg_show",
-						kind = "",
-						find = "written",
-					},
-					opts = { skip = true },
-				},
-				{
-					filter = {
-						event = "msg_show",
-						kind = "",
-						find = "change",
-					},
-					opts = { skip = true },
-				},
-			},
-		})
-	end,
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+    },
+    opts = {
+        cmdline = {
+            -- enabled = true,
+            view = "cmdline",
+        },
+        notify = {
+            enabled = false,
+            view = "notify",
+        },
+        presets = {
+            bottom_search = true,
+            command_palette = false,
+            long_message_to_split = true,
+            inc_rename = false,
+            lsp_doc_border = true,
+        },
+        routes = {
+            { filter = { event = "msg_show", kind = "", find = "written" }, opts = { skip = true } },
+            { filter = { event = "msg_show", kind = "", find = "change" }, opts = { skip = true } },
+        },
+    },
 }
