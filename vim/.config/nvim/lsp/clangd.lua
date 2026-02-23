d27bb58324cc5cf2ae93da17d430b9af8e7b4922
Jeffrey Sulton <jeffrey.sulton@dtex.ai>
2026-02-23 11:39:40 -0800

    fix(nvim): add query-driver to clangd for Xcode SDK header resolution

--- vim/.config/nvim/lsp/clangd.lua
+++ vim/.config/nvim/lsp/clangd.lua
@@ -4,6 +4,7 @@ return {
 		"--clang-tidy",
 		"--background-index",
 		"--offset-encoding=utf-8",
+        "--query-driver=/Applications/Xcode*.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang*",
 	},
 	root_markers = { ".clangd", "buildServer.json", ".xcodeproj", ".xcworkspace" },
 	filetypes = { "c", "cpp", "objc", "objcpp" },
