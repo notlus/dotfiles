fb0e5c87d581bed83bbaf5c4a25b771e2b665b65
Jeffrey Sulton <jeffrey.sulton@dtex.ai>
2026-02-23 11:34:47 -0800

    style(nvim): convert indentation from tabs to spaces

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -4,84 +4,84 @@ vim.g.maplocalleader = " "
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 ---@diagnostic disable-next-line: undefined-field
 if not vim.uv.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Load lazy.nvim and its plugins
 
 require("lazy").setup("plugins", {
-	"folke/which-key.nvim",
-	{ "folke/neoconf.nvim", cmd = "Neoconf" },
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-		change_detection = {
-			enabled = true, -- automatically check for config file changes and reload the ui
-			notify = false, -- turn off notifications whenever plugin changes are made
-		},
-		config = function()
-			vim.cmd([[colorscheme tokyonight]])
-		end,
-	},
+    "folke/which-key.nvim",
+    { "folke/neoconf.nvim", cmd = "Neoconf" },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        change_detection = {
+            enabled = true, -- automatically check for config file changes and reload the ui
+            notify = false, -- turn off notifications whenever plugin changes are made
+        },
+        config = function()
+            vim.cmd([[colorscheme tokyonight]])
+        end,
+    },
 
-	require("core.options"),
-	require("core.keymaps"),
-	require("lsp"),
+    require("core.options"),
+    require("core.keymaps"),
+    require("lsp"),
 })
 
 -- Highlight configurations
 vim.cmd([[highlight Normal guibg=none]])
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "c", "cpp", "objc", "objcpp", "swift" },
-	callback = function()
-		vim.opt_local.commentstring = "// %s"
-	end,
+    pattern = { "c", "cpp", "objc", "objcpp", "swift" },
+    callback = function()
+        vim.opt_local.commentstring = "// %s"
+    end,
 })
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
-	callback = function()
-		vim.hl.on_yank()
-	end,
+    group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+    callback = function()
+        vim.hl.on_yank()
+    end,
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = { "gitcommit", "markdown" },
-	callback = function()
-		vim.opt_local.wrap = true
-		vim.opt_local.spell = true
-	end,
+    pattern = { "gitcommit", "markdown" },
+    callback = function()
+        vim.opt_local.wrap = true
+        vim.opt_local.spell = true
+    end,
 })
 
 vim.api.nvim_create_autocmd("User", {
-	pattern = { "XcodebuildBuildFinished", "XcodebuildTestsFinished" },
-	callback = function(event)
-		if event.data.cancelled then
-			return
-		end
+    pattern = { "XcodebuildBuildFinished", "XcodebuildTestsFinished" },
+    callback = function(event)
+        if event.data.cancelled then
+            return
+        end
 
-		if event.data.success then
-			require("trouble").close()
-		elseif not event.data.failedCount or event.data.failedCount > 0 then
-			if next(vim.fn.getqflist()) then
-				require("trouble").open("quickfix")
-			else
-				require("trouble").close()
-			end
+        if event.data.success then
+            require("trouble").close()
+        elseif not event.data.failedCount or event.data.failedCount > 0 then
+            if next(vim.fn.getqflist()) then
+                require("trouble").open("quickfix")
+            else
+                require("trouble").close()
+            end
 
-			require("trouble").refresh()
-		end
-	end,
+            require("trouble").refresh()
+        end
+    end,
 })
