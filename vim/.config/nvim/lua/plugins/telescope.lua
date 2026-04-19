32dd9feef3fb4fe3fcd0adc938da4e5d3cfc5911
Jeffrey Sulton <jeffrey.sulton@dtex.ai>
2026-04-18 21:39:05 -0700

    style(nvim): reindent `telescope` config

--- vim/.config/nvim/lua/plugins/telescope.lua
+++ vim/.config/nvim/lua/plugins/telescope.lua
@@ -1,66 +1,66 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope-fzf-native.nvim",
-		{ "nvim-telescope/telescope-ui-select.nvim" },
-		build = "make",
-	},
-	config = function()
-		local icons = require("core.icons")
+    "nvim-telescope/telescope.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-fzf-native.nvim",
+        { "nvim-telescope/telescope-ui-select.nvim" },
+        build = "make",
+    },
+    config = function()
+        local icons = require("core.icons")
 
-		require("telescope").setup({
-			defaults = {
-				prompt_prefix = icons.ui.Telescope .. " ",
-				selection_caret = icons.ui.Forward .. " ",
-				entry_prefix = "   ",
-				initial_mode = "insert",
-				selection_strategy = "reset",
-				path_display = { "smart" },
-				color_devicons = true,
-				mappings = {
-					i = {
-						["<C-j>"] = require("telescope.actions").move_selection_next,
-						["<C-k>"] = require("telescope.actions").move_selection_previous,
-					},
-					n = {
-						["<C-j>"] = require("telescope.actions").move_selection_next,
-						["<C-k>"] = require("telescope.actions").move_selection_previous,
-					},
-				},
-			},
-			extensions = {
-				["ui-select"] = {
-					require("telescope.themes").get_dropdown(),
-				},
-				fzf = {
-					fuzzy = true, -- false will only do exact matching
-					override_generic_sorter = true, -- override the generic sorter
-					override_file_sorter = true, -- override the file sorter
-					case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-				},
-			},
-			pickers = {
-				find_files = {
-					theme = "dropdown",
-					previewer = false,
-				},
-				buffers = {
-					theme = "dropdown",
-					previewer = false,
-				},
-				lsp_references = {
-					theme = "dropdown",
-				},
-				lsp_document_symbols = {
-					theme = "dropdown",
-				},
-			},
-		})
+        require("telescope").setup({
+            defaults = {
+                prompt_prefix = icons.ui.Telescope .. " ",
+                selection_caret = icons.ui.Forward .. " ",
+                entry_prefix = "   ",
+                initial_mode = "insert",
+                selection_strategy = "reset",
+                path_display = { "smart" },
+                color_devicons = true,
+                mappings = {
+                    i = {
+                        ["<C-j>"] = require("telescope.actions").move_selection_next,
+                        ["<C-k>"] = require("telescope.actions").move_selection_previous,
+                    },
+                    n = {
+                        ["<C-j>"] = require("telescope.actions").move_selection_next,
+                        ["<C-k>"] = require("telescope.actions").move_selection_previous,
+                    },
+                },
+            },
+            extensions = {
+                ["ui-select"] = {
+                    require("telescope.themes").get_dropdown(),
+                },
+                fzf = {
+                    fuzzy = true, -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    override_file_sorter = true, -- override the file sorter
+                    case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+                },
+            },
+            pickers = {
+                find_files = {
+                    theme = "dropdown",
+                    previewer = false,
+                },
+                buffers = {
+                    theme = "dropdown",
+                    previewer = false,
+                },
+                lsp_references = {
+                    theme = "dropdown",
+                },
+                lsp_document_symbols = {
+                    theme = "dropdown",
+                },
+            },
+        })
 
-		pcall(require("telescope").load_extension, "fzf")
-		pcall(require("telescope").load_extension, "ui-select")
-	end,
-	cmd = { "Telescope" },
+        pcall(require("telescope").load_extension, "fzf")
+        pcall(require("telescope").load_extension, "ui-select")
+    end,
+    cmd = { "Telescope" },
 }
